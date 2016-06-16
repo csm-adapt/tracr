5e9fe301db058945c950959030a947c78f1ab053
Branden Kappes <bkappes@mines.edu>
2016-06-16 11:49:35 -0600

    2016-06-16 11:48, B. Kappes
    Changed directory BASE to STRUCTURES to better reflect its function.

---
+++ tracr/structures/porosity.py
@@ -0,0 +1,34 @@
+from scipy.ndimage.measurements import label
+
+class Porosity(object)
+    """
+    Accepts a 3D intensity array and determines the pore/void
+    locations.
+    """
+    def __init__(self, arr, *args, **kwds):
+        # intensity array
+        self.set_array(arr)
+
+    def __iter__(self):
+        for ilab in xrange(self.nlabels):
+            yield (self.labels == ilab)
+
+    def set_array(self, arr, **kwds):
+        kwds['structure'] = kwds.get('structure', np.ones((3,3,3)))
+        self._arr = np.asarray(arr)
+        labels, num = label(self._arr, **kwds)
+        self._labels = labels
+        self._nlabels = num
+
+    @property
+    def array(self):
+        return self._arr
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def nlabels(self):
+        return self._nlabels
+#end 'class Pore(object)'
