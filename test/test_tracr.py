b1bcc11da31366224e0da14d28dcd52a34375c4d
Branden Kappes <bkappes@mines.edu>
2016-06-17 15:30:24 -0600

    2016-06-17 15:30, B. Kappes
    Wrote nose tests for distribution.orientation.py. orientation.principal returns the principal axes.
    
    Reveals need for axis utilities.

--- test/test_tracr.py
+++ test/test_tracr.py
@@ -1,29 +1,28 @@
 import sys
 sys.path.append('..')
-from NAME.MODULE import METHOD
+#from tracr import tracr
 
 # To test, simply run
 # [...]$ nosetests (optionally with -v)
 # and a report a summary of the results
 
 class TestClass: # keep this the same
-	def setUp(self):
-		# construct objects and perform any necessary setup
-		pass
+    def setUp(self):
+        # construct objects and perform any necessary setup
+        pass
 
-	def test_UNIT_1(self):
-		# run test one
-		assert 1 == 1 # assertion, if false, fails
-		pass
+    def test_UNIT_1(self):
+        # run test one
+        assert 1 == 1 # assertion, if false, fails
+        pass
 
-	def test_UNIT_2(self):
-		# run test two
-		assert 2 == 2 # assertion, if false, fails
-		pass
+    def test_UNIT_2(self):
+        # run test two
+        assert 2 == 2 # assertion, if false, fails
+        pass
 
-	# ...
+    # ...
 
-	def tearDown(self):
-		# clean up
-		pass
-
+    def tearDown(self):
+        # clean up
+        pass
