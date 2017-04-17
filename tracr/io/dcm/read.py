d9bec5c7a135d3262bef161433f6010998891781
hgeerlings <henrygeerlings@gmail.com>
2017-04-17 05:40:01 -0600

    Add DCM reader and update requirements.txt for pydicom

---
+++ tracr/io/dcm/read.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+A script that converts DCM formatted data into a numpy array of intensity
+values. Iteratively processes each .dcm layer at a time (no multilayer).
+
+INPUT:
+	- Either a single .dcm file or folder of .dcm frames
+		e.g. read('path/to/DCM_folder/')
+
+OUTPUT:
+	- Numpy array (either 2D or 3D) of .dcm intensity data. 3D arrays transposed
+		for 'z' = upward indexing
+		e.g. read('path/to/frame.dcm')
+"""
+
+import sys, os, glob
+import dicom
+import numpy as np
+
+def read_single(ifile):
+    return dicom.read_file(ifile).pixel_array
+
+def read(ifile):
+	# If input is folder, iterate through each frame and then transpose 3D array
+    if os.path.isdir(ifile):
+        all_frames = glob.glob(ifile+'*')
+        return np.transpose(np.array([read_single(frame) for frame in all_frames]),
+                                        axes=(1,2,0))
+	else:
+        return read_single(ifile)
+
+if __name__ == '__main__':
+	try:
+		# generate array
+		intensity_array = read(ifile)
+		# read filename
+		try:
+			ofile = sys.argv[2]
+		except IndexError:
+			path, base = os.path.split(ifile)
+			ofile, ext = os.path.splitext(base)
+		# save the output
+		np.save(path+ofile, intensity_array)
+	except:
+		sys.stderr.write('Usage: {} INPUT.tif [OUTPUT.npy]'.format(sys.argv[0]))
+		sys.exit(1)
---
+++ tracr/io/dcm/write.py
@@ -0,0 +1,6 @@
+#
+# Writes a TIF file from a numpy array
+#
+
+def write(filename, arr):
+    raise NotImplementedError('Writing TIF files is not yet available.')
