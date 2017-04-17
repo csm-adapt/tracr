af1a57da7c1cd7ba54bafc280ec6693edde42dec
hgeerlings <henrygeerlings@gmail.com>
2017-04-17 07:52:21 -0600

    Add dicom file guesser and data folder reader.
    Not sure how keywords and second file ‘checker’ will be used so kept
    them in. Added ability for reader to detect folder and use containing
    data as the guessed file. Also included dcm guessing.

--- tracr/io/read.py
+++ tracr/io/read.py
@@ -1,11 +1,41 @@
 # tries to guess the format of the file
+"""
+Read in TIF or DCM intensity data and send to appropriate reader depending on
+format. Assume that a folder input contains data frames directly below
+(i.e. there are no nested folders.)
+
+INPUT:
+    - TIF: file (single or multilayer), or folder of TIF frames
+    - DICOM: file (single), or folder of DCM frames
+
+OUTPUT:
+    - Numpy array of intensity data. See specific readers for more.
+
+USAGE:
+    e.g. read('path/to/DataFolder/')
+    e.g. read('path/to/sampleX.dcm')
+    e.g. read('path/to/sampleX.tif')
+"""
+
+
 from .util import guess_format, check_format
 from .tif import read as read_tif
+from .dcm import read as read_dcm
 
 def read(filename, **kwds):
-    fmt = kwds.get('format', guess_format(filename))
-    fmt = check_format(fmt)
+    # Not sure what keywords we would pass here...
+    #fmt = kwds.get('format', guess_format(filename))
+    #fmt = check_format(fmt)
+    
+    ## Check if user input is a file or folder of files
+    if os.path.isdir(filename):
+        fmt = guess_format(os.listdir(filename)[0])
+    else:
+        fmt = guess_format(filename)
+    # Select appropriate reader
     if fmt == 'tif':
         return read_tif(filename)
+    elif fmt == 'dcm':
+        return read_dcm(filename)
     else:
         raise RuntimeError('You should never get here!')
--- tracr/io/util.py
+++ tracr/io/util.py
@@ -1,23 +1,35 @@
+"""
+File reading tools for decomposing input file name. Returns format string
+depending on extension (either '.tif/.tiff' or '.dcm/.dicom').
+
+INPUT:
+    - string containing file name (no folders make it here)
+
+OUTPUT:
+    - string declaring extension (tif or dcm)
+"""
+
 import os
 
-
 def guess_format(filename):
-    # guess the format of the file
+    # Guess the format of the file
     pathstr, basename = os.path.split(filename)
     name, ext = os.path.splitext(basename)
     ext = ext.lower()
     # list recognized filenames
     if ext in ('.tif', '.tiff'):
         return 'tif'
+    elif ext in ('.dcm', '.dicom')
+        return 'dcm'
     else:
         msg = 'Could not guess the format of {}.'.format(filename)
         raise IOError(msg)
 
-
+# What is the point of this block?
 def check_format(fmt):
     fmt = fmt.lower()
     if ext in ('tif', 'tiff'):
-        return 'tif'
+        return 'tif'    
     else:
         msg = '{} format is not a recognized format.'.format(fmt)
         raise IOError(msg)
