caf5efe25cf23e5c8bbd46280dafc1b7975b112c
hgeerlings <henrygeerlings@gmail.com>
2017-04-20 10:13:59 -0600

    Move 'folder check' to actual file guesser function to allow use of keyword input.

--- tracr/io/read.py
+++ tracr/io/read.py
@@ -1,8 +1,9 @@
 # tries to guess the format of the file
 """
 Read in TIF or DCM intensity data and send to appropriate reader depending on
-format. Assume that a folder input contains data frames directly below
-(i.e. there are no nested folders.)
+format. So far, this assumes that a folder input contains data frames directly
+below (i.e. there are no nested folders.) and that contained files are listed
+sequentially.
 
 INPUT:
     - TIF: file (single or multilayer), or folder of TIF frames
@@ -23,15 +24,8 @@ from .tif import read as read_tif
 from .dcm import read as read_dcm
 
 def read(filename, **kwds):
-    # Not sure what keywords we would pass here...
-    #fmt = kwds.get('format', guess_format(filename))
-    #fmt = check_format(fmt)
-
-    ## Check if user input is a file or folder of files
-    if os.path.isdir(filename):
-        fmt = guess_format(os.listdir(filename)[0])
-    else:
-        fmt = guess_format(filename)
+    # If format specified, great. If not, we call guess_format.
+    fmt = kwds.get('format', guess_format(filename))
     # Select appropriate reader
     if fmt == 'tif':
         return read_tif(filename)
--- tracr/io/util.py
+++ tracr/io/util.py
@@ -3,7 +3,7 @@ File reading tools for decomposing input file name. Returns format string
 depending on extension (either '.tif/.tiff' or '.dcm/.dicom').
 
 INPUT:
-    - string containing file name (no folders make it here)
+    - string containing file name. If folder, we access first file.
 
 OUTPUT:
     - string declaring extension (tif or dcm)
@@ -12,8 +12,12 @@ OUTPUT:
 import os
 
 def guess_format(filename):
+    # Check if input is directory or not. If so, grab first file within.
+    if os.path.isdir(filename):
+        pathstr, basename = os.path.split(filename[0])
+    else:
+        pathstr, basename = os.path.split(filename)
     # Guess the format of the file
-    pathstr, basename = os.path.split(filename)
     name, ext = os.path.splitext(basename)
     ext = ext.lower()
     # list recognized filenames
