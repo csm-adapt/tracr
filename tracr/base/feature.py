cba5a3f42235a9e61759bb88b2bb8d3259db6d9b
hgeerlings <henrygeerlings@gmail.com>
2017-04-27 11:59:35 -0600

    Initiate Feature class, will add more attributes shortly, still WIP

---
+++ tracr/base/feature.py
@@ -0,0 +1,21 @@
+"""
+Generation of our 'Feature' object. This class accepts a numpy array of the
+raw intensity data (read in from tracr.io.read). It extends the ndarray class
+by further attributing: label count, volumes, COMs, etc.
+"""
+# Will add more thresholding schemes once we clean up thresholding functions:
+from skimage.filters import threshold_otsu as skotsu
+from scipy.ndimage.measurements import label, center_of_mass
+
+class Feature(object):
+    # Where 'object' represents the numpy array of pre-read intensity data
+    # This object is initiated within tracr.io.read
+    def __init__(self, arr, *args, **kwds):
+        self.set_array(arr, **kwds)
+
+    def set_array(self, arr, **kwds):
+        self.thresh_val = self.threshold(arr)
+        self.labels, self.nlabels = label((arr<self.thresh_val))
+
+    def threshold(self, arr):
+        return skotsu(arr.flatten())
