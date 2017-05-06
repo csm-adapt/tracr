65f50445b56ef74fce7279988ae48d65418be7ca
hgeerlings <henrygeerlings@gmail.com>
2017-05-06 12:29:25 -0600

    Update testing for single tif and dcm frames. Should be functional although difficult to test with old master branch.

--- test/test_read.py
+++ test/test_read.py
@@ -5,16 +5,22 @@ FOR NOW: Running test on 2D sample, will scale up to data shortly
 
 import os, sys, glob
 sys.path.append('..')
-from tracr.io.tif.read import read_single as read_singleTIF
-from tracr.io.tif.read import read_multilayer as read_multiTIF
+from tracr.io.tif import read as readTIF
+from tracr.io.dcm import read as readDCM
+from matplotlib import pyplot as plt
 
+# Only using single files for now, not desirable to store huge folders
 class TestReadTIF:
 
     def test_singleTIF(self):
         data = 'testing_data/P002_B001_C04-0.4X_4.83122_0666.tif'
-        arr = read_singleTIF(data)
-        # Make assertions, tests, etc.
+        feat = readTIF(data)
 
+        # Make assertions, tests, etc.
+        assert type(feat) is Feature
+        print feat.nlabels
+        plt.figure()
+        plt.imshow(feat.labels)
     # Insert multilayer tif reading tests
 
     # Insert tif folder tests
@@ -24,10 +30,17 @@ class TestReadTIF:
 
 
 class TestReadDCM:
+    def test_singleDCM(self):
+        data = 'testing_data/P002_B001_C04-0.4X_4.83122um_.dcm0666.dcm'
+        feat = readDCM(data)
+
+        # Make assertions, tests, etc.
+        assert type(feat) is Feature
+        print feat.nlabels
+        plt.figure()
+        plt.imshow(feat.labels)
+
 
     # Insert single dcm reading tests (testing tracr.io.dcm.read)
 
     # Insert dcm folder reading tests
-
-class TestReadANY:
-    # Insert test for either tif or dcm (testing tracr.io.read)
