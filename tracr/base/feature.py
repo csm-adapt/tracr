886cabb8616a1f9740668efd356c6c8896391e57
hgeerlings <henrygeerlings@gmail.com>
2017-05-08 08:52:33 -0600

    Add __array_finalize__, also missing pixelsize flag

--- tracr/base/feature.py
+++ tracr/base/feature.py
@@ -4,13 +4,12 @@ raw intensity data (read in from tracr.io.read). It extends the ndarray class
 by further attributing: label count, volumes, COMs, etc.
 """
 # Will add more thresholding schemes once we clean up thresholding functions:
-from skimage.filters import threshold_otsu as skotsu
-from scipy.ndimage.measurements import label, center_of_mass
+import numpy as np
 
-class Feature(object):
+class Feature(np.ndarray):
     # Where 'object' represents the numpy array of pre-read intensity data
-    # This object is initiated within tracr.io.read
-    def __init__(cls, arr, **kwds):
+    # create a new Feature object as a view
+    def __new__(cls, arr, **kwds):
         #self.set_array(arr, **kwds)
         obj = np.asarray(arr).view(cls)
 
@@ -18,9 +17,14 @@ class Feature(object):
         obj.pixelsize = kwds.get('pixelsize', None)
         return obj
 
-    #def set_array(self, arr, **kwds):
-        #self.thresh_val = self.threshold(arr)
-        #self.labels, self.nlabels = label((arr < self.thresh_val))
+    def __array_finalize__(self, obj):
 
-    #def threshold(self, arr):
-        #return skotsu(arr.flatten())
+        # when creating a new instance, this is called before the
+        # instance is actually created (i.e. when `__new__` is called)
+        if obj is None:
+            return
+
+        if 'pixelsize' not in kwds:
+            msg = "'pixelsize' was not set when Feature object was created. " \
+                "A pixel size of 1 is assumed for all calculations."
+            tracrlog.warning(msg)
