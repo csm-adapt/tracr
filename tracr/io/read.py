91a64e49635ca4059dd4b6baf2e062fe5195d35f
hgeerlings <henrygeerlings@gmail.com>
2017-05-15 17:54:02 -0600

    Logging of unspecified pixel size has been moved here. Also check for mixed file types - converting any input into a list type.

--- tracr/io/read.py
+++ tracr/io/read.py
@@ -19,31 +19,49 @@ USAGE:
     e.g. read('path/to/sampleX.tif', pixelsize=4.5)
 """
 
-import os, glob
+import os
+import logging
+logging.basicConfig(filename='feature.log', level=logging.DEBUG)
 from .util import guess_format
 from .tif import read as read_tif
 from .dcm import read as read_dcm
 
-def read(filename, **kwds):
-    # If format specified, great (still lower it). If not, we call guesser.
+def read(ifile, **kwds):
+    # Determine whether input is file or list of files, and ensure output is list
+    try:
+        # Turn single file (single or multilayer) into a list
+        if os.path.isfile(ifile):
+            filenames = [ifile]
+    except TypeError:
+        # Input is a user-specified list of frames
+        if hasattr(ifile, '__iter__'):
+            filenames = ifile
+        else:
+            raise
 
-    # Check if input is iterable (e.g. list of files sent by user)
-    # Note: os.path.isfile(iterable) throws error so this comes in the 'if'
-    if hasattr(filename, '__iter__'):
-        fmt = kwds.get('format', guess_format(filename[0]))
-    # If not, input is a single frame or multialyer file
-    else:
-        fmt = kwds.get('format', guess_format(filename))
+    # Collect and check for mixed file types in list
+    formats = [kwds.get('format', guess_format(f)) for f in filenames]
+    num_formats = len(list(set(formats)))
+    if num_formats != 1:
+        # Only print first list item to prevent explosion
+        msg = 'More than one file format found in {}'.format(filenames[0])
+        raise IOError(msg)
 
-    # Check for pixel size keyword. Set to '1' if unspecified. This will log
-    # in Feature.
-    px_size = kwds.get('pixelsize', 1)
+    # Check for specified pixelsize, warn if not specified.
+    try:
+        px_size = kwds['pixelsize']
+    except KeyError:
+        msg = 'Pixel size was not specified and is not provided in {}. ' \
+                'Default to 1.'.format(filenames[0])
+        print msg
+        px_size = 1.0
 
-    # Select appropriate reader based on 'fmt' - they will check if file/folder
+    # Select appropriate reader based on 'fmt' - they will check if file/folder.
+    # 'pixelsize' is a set **kwds argument from here on out.
     if fmt.lower() in ('tif', 'tiff'):
-        return read_tif(filename, pixelsize=px_size)
+        return read_tif(filenames, pixelsize=px_size)
     elif fmt.lower() in ('dcm', 'dicom'):
-        return read_dcm(filename, pixelsize=px_size)
+        return read_dcm(filenames, pixelsize=px_size)
     else:
         msg = '{} is not a recognized input format.'.format(fmt)
         raise NotImplementedError(msg)
