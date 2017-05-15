f21e942caa16c1abcae1e91d8d5bcc4e46d5e954
hgeerlings <henrygeerlings@gmail.com>
2017-05-15 16:35:35 -0600

    Replace folder reading function with list reading function. Also incorporate pixel size info, value is raised to '1' here, and logged in Feature if px_size==1.

--- tracr/io/read.py
+++ tracr/io/read.py
@@ -4,20 +4,19 @@ Read in TIF or DCM intensity data and send to appropriate reader depending on
 format. If keyword arg specified for 'format', this is just a wrapper for the
 readers. If not, we guess. So far, this assumes that a folder input contains
 data frames directly below (i.e. there are no nested folders.) and that
-contained files are listed sequentially.
+contained files are listed sequentially. Unspecified pixel size is set to 1.
 
 INPUT:
-    - TIF: file (single or multilayer), or folder of TIF frames
-    - DICOM: file (single), or folder of DCM frames
+    - TIF: file (single or multilayer), or user specified list of frames
+    - DICOM: file (single), or list of dcm frames
 
 OUTPUT:
-    - Numpy array of intensity data. See specific readers for more.
+    - Feature object of intensity data. See specific readers for more.
 
 USAGE:
-    e.g. read('path/to/DataFolder/', {'format' : 'tif'})
-        (no guessing happens for the above)
-    e.g. read('path/to/sampleX.dcm')
-    e.g. read('path/to/sampleX.tif')
+    e.g. read(glob.glob('myDataFolder/*tif'))
+    e.g. read('path/to/sampleX.dcm', format='dcm'), (no guessing here)
+    e.g. read('path/to/sampleX.tif', pixelsize=4.5)
 """
 
 import os, glob
@@ -26,17 +25,18 @@ from .tif import read as read_tif
 from .dcm import read as read_dcm
 
 def read(filename, **kwds):
-    # If format specified, great (still lower it). If not, we call
-    #   guess_format using first file in folder.
+    # If format specified, great (still lower it). If not, we call guesser.
 
-    # Make sure only a single file is sent to the reader to check format
-    if os.path.isdir(filename):
-        frame = glob.glob(filename + '*')[0]
-        fmt = kwds.get('format', guess_format(frame))
+    # Check if input is iterable (e.g. list of files sent by user)
+    # Note: os.path.isfile(iterable) throws error so this comes in the 'if'
+    if hasattr(filename, '__iter__'):
+        fmt = kwds.get('format', guess_format(filename[0]))
+    # If not, input is a single frame or multialyer file
     else:
         fmt = kwds.get('format', guess_format(filename))
 
-    # Check for pixel size keyword. Set to '1' if unspecified
+    # Check for pixel size keyword. Set to '1' if unspecified. This will log
+    # in Feature.
     px_size = kwds.get('pixelsize', 1)
 
     # Select appropriate reader based on 'fmt' - they will check if file/folder
