16f554fd5b41c9cda539bf3eca23fb1a3e1c59d5
hgeerlings <henrygeerlings@gmail.com>
2017-05-15 18:50:46 -0600

    Update test for checking specified/unspecified pixel sizes. So far only for single frame tif and single frame dcm.

--- test/test_read.py
+++ test/test_read.py
@@ -5,16 +5,17 @@ FOR NOW: Running test on 2D sample, will scale up to data shortly
 
 import os, sys, glob
 sys.path.append('..')
+from tracr.io.read import read
 from tracr.io.tif import read as readTIF
 from tracr.io.dcm import read as readDCM
 from matplotlib import pyplot as plt
 
 # Only using single files for now, not desirable to store huge folders
 class TestReadTIF:
-
-    def test_singleXCT_TIF(self):
+    # Do not specify pixel size
+    def test_singleTIF(self):
         data = 'testing_data/P002_B001_C04-0.4X_4.83122_0666.tif'
-        feat = readTIF(data)
+        feat = read(data)
 
         # Make assertions, tests, etc.
         assert type(feat) is Feature, \
@@ -22,30 +23,53 @@ class TestReadTIF:
         assert np.sum(feat) == 9953421973, \
             'Checksum of Feature is: {} ' \
             '(should be 9953421973)'.format(np.sum(feat))
-    # Insert multilayer tif reading tests
+        assert hasattr(feat, 'pixelsize'), 'No pixelsize attribute'
+        assert feat.pixelsize == 1.0, 'Pixelsize not set to 1'
 
-    # Insert tif folder tests
-    def test_singleMET_TIF(self):
-        data = 'testing_data/P002_B001_O14_500x_layer3_capture.tif'
-        feat = readTIF(data)
-
-        assert type(feat) is Feature, \
-            'Feature is of type: {}'.format(type(feat))
-        assert np.sum(feat) == 142991265, \
-            'Checksum of Feature is: {} ' \
-            '(should be 142991265)'.format(np.sum(feat))
-
-class TestReadDCM:
-    def test_singleDCM(self):
-        data = 'testing_data/P002_B001_C04-0.4X_4.83122um_.dcm0666.dcm'
-        feat = readDCM(data)
+    # Specify pixel size
+    def test_singleTIF_px(self):
+        data = 'testing_data/P002_B001_C04-0.4X_4.83122_0666.tif'
+        feat = read(data, pixelsize=4.5)
 
         # Make assertions, tests, etc.
         assert type(feat) is Feature, \
             'Feature is of type: {}'.format(type(feat))
-        assert np.sum(feat.raw_arr) == 9953421973, \
+        assert np.sum(feat) == 9953421973, \
             'Checksum of Feature is: {} ' \
             '(should be 9953421973)'.format(np.sum(feat))
+        assert hasattr(feat, 'pixelsize'), 'No pixelsize attribute'
+        assert feat.pixelsize == 4.5, 'Pixelsize not as specified'
+
+
+    # Insert multilayer tif reading tests:
+
+
+class TestReadDCM:
+    def test_singleDCM(self):
+        data = 'testing_data/P002_B001_C04-0.4X_4.83122um_.dcm0666.dcm'
+        feat = readALL(data)
+
+        # Make assertions, tests, etc.
+        assert type(feat) is Feature, \
+            'Feature is of type: {}'.format(type(feat))
+        assert np.sum(feat) == 9953421973, \
+            'Checksum of Feature is: {} ' \
+            '(should be 9953421973)'.format(np.sum(feat))
+        assert hasattr(feat, 'pixelsize'), 'No pixelsize attribute'
+        assert feat.pixelsize == 1.0, 'Pixel size not set to 1'
+
+    def test_singleDCM_px(self):
+        data = 'testing_data/P002_B001_C04-0.4X_4.83122um_.dcm0666.dcm'
+        feat = readALL(data, pixelsize=4.5)
+
+        # Make assertions, tests, etc.
+        assert type(feat) is Feature, \
+            'Feature is of type: {}'.format(type(feat))
+        assert np.sum(feat) == 9953421973, \
+            'Checksum of Feature is: {} ' \
+            '(should be 9953421973)'.format(np.sum(feat))
+        assert hasattr(feat, 'pixelsize'), 'No pixelsize attribute'
+        assert feat.pixelsize == 4.5, 'Pixel size not as specified'
 
 
     # Insert single dcm reading tests (testing tracr.io.dcm.read)
