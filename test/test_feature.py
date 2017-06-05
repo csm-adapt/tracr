e45a2c9626887433199815bd6686787cef8281f6
hgeerlings <henrygeerlings@gmail.com>
2017-06-05 12:28:49 -0600

    Updated test script to catch warnings for unspecified pixel size. Please see 'scope' for generating toy array, is this correct usage (works, but unsure why).

--- test/test_feature.py
+++ test/test_feature.py
@@ -1,35 +1,55 @@
 """
 Test script for TRACR Feature objects.
-FOR NOW: Testing type and pixelsize attribute
+    - Uses generic ndarray to wrap into Feature
 """
 
 import os, sys, glob
 import numpy as np
+import pytest
 sys.path.append('..')
 from tracr.base.feature import Feature
-from tracr.io.read import read
+import warnings
+warnings.simplefilter("always")
 
-class TestFeature:
 
-    def test_toy_array(self):
+@pytest.fixture(scope="class")
+def toy():
+    return np.resize(np.arange(25), (5,5))
+
+class TestToyArray:
+
+    def test_pixel_size(self, toy):
         # Start with specified pixelsize (should raise nothing)
-        frame = np.resize(np.arange(25), (5,5))
-        feat = Feature(frame, pixelsize=1.234)
+        px_size = 1.234
+        feat = Feature(toy, pixelsize=px_size)
 
+        # Assert object is of type Feature
         assert type(feat) is Feature, \
             'Feature is of type: {}'.format(type(feat))
-        # figure out asseriton for NOT raising log warning (testfixtures??)
+        # Assert a 'pixelsize' attribute
+        assert hasattr(feat, 'pixelsize'), \
+            "Feature is missing 'pixelsize' attribute."
+        # Assert pixelsize is as specified
+        assert feat.pixelsize == 1.234, \
+            "Actual pixelsize ({}) not match specified pixelsize ({})".format(
+                feat.pixelsize, px_size)
 
-        # Try new frame without pizelsize
-        feat2 = Feature(frame)
+    def test_no_pixel_size(self, toy):
+        # Make sure warning is thrown without specified pixelsize
+        warnings.filterwarnings("error")
+        with pytest.raises(UserWarning):
+            feat = Feature(toy)
+        warnings.resetwarnings()
 
+        # Above will fail, this will just give visual warnings
+        feat = Feature(toy)
+        # Assert object is of type Feature
         assert type(feat) is Feature, \
             'Feature is of type: {}'.format(type(feat))
-        # Assert that warning WAS RAISED
-
-    def test_XCT_layer(self):
-        # Read already expected to return a Feature object, how to send px size?
-        feat = read('testing_data/P002_B001_C04-0.4X_4.83122_0666.tif')
-
-        assert type(feat) is Feature, \
-            'Feature is of type: {}'.format(type(feat))
+        # Assert a 'pixelsize' attribute
+        assert hasattr(feat, 'pixelsize'), \
+            "Feature is missing 'pixelsize' attribute."
+        # Assert pixelsize is as specified
+        assert feat.pixelsize == 1, \
+            "Actual pixelsize ({}) not match specified pixelsize ({})".format(
+                feat.pixelsize, 1)
--- tracr/base/feature.py
+++ tracr/base/feature.py
@@ -1,25 +1,29 @@
 """
 Generation of our 'Feature' object. This class accepts a numpy array of the
 raw intensity data (read in from tracr.io.read). It extends the ndarray class
-by further attributing: label count, volumes, COMs, etc.
+by further attributing: pixel size, (more to come).
 """
-# Will add more thresholding schemes once we clean up thresholding functions:
+
 import numpy as np
 import logging
+from warnings import warn
 logging.basicConfig(filename='feature.log', level=logging.DEBUG)
 
 class Feature(np.ndarray):
     # Where 'object' represents the numpy array of pre-read intensity data
-    # create a new Feature object as a view
     def __new__(cls, arr, **kwds):
-        #self.set_array(arr, **kwds)
+        # Create a new Feature object as a view
         obj = np.asarray(arr).view(cls)
 
         # Check for pixel size
         if 'pixelsize' not in kwds:
             msg = "'pixelsize' was not set when Feature object was created. " \
-                "A pixel size of 1 is assumed for all calculations (pixel dim's)."
-            logging.warning(msg)
+                "A pixel size of 1 is assumed for all calculations."
+            warn(msg)
+
+        # Record pixelsize in log file
+        obj.pixelsize = kwds.get('pixelsize', 1)
+        logging.info("Pixel size: {} um".format(obj.pixelsize))
         return obj
 
     def __array_finalize__(self, obj):
@@ -28,4 +32,4 @@ class Feature(np.ndarray):
         # instance is actually created (i.e. when `__new__` is called)
         if obj is None:
             return
-        self.pixelsize = getattr(obj, 'pixelsize', 1)
+        #self.pixelsize = getattr(obj, 'pixelsize', 1)
