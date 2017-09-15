b09cc71415c109d2e306ae64de2ff8316a98c6d4
hgeerlings <henrygeerlings@gmail.com>
2017-09-14 19:22:53 -0600

    Add volume calculator to volumes.py

--- tracr/metrics/volume.py
+++ tracr/metrics/volume.py
@@ -1,9 +1,19 @@
 # Returns a list of void volumes (pixels) in order determined by labeled array.
 import numpy as np
 
-def pore_volume(porosity):
-    vols = np.array([
-        np.sum(p)
-        for p in porosity
-    ])
+def volumes(larr):
+"""
+INPUT
+=====
+    larr (ndarray): A labeled array with 'num' clusters of integer values and '0'
+                    cluster for passed material (background).
+
+OUTPUT
+======
+    vols (list): An 'n+1' list of voxel volumes corresponding to labels from 'arr'.
+
+"""
+    num = larr.max()
+    vols = np.zeros(num+1)
+    np.add.at(vols, larr.flat, 1)
     return vols
